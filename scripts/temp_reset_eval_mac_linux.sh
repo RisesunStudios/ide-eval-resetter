4206ce695102fe23be0fd6217640675258ba4eff
pengzhile <pengzhile@gmail.com>
2021-10-08 10:08:10 +0800

    fix scripts

--- scripts/temp_reset_eval_mac_linux.sh
+++ scripts/temp_reset_eval_mac_linux.sh
@@ -4,21 +4,23 @@
 OS_NAME=$(uname -s)
 JB_PRODUCTS="IntelliJIdea CLion PhpStorm GoLand PyCharm WebStorm Rider DataGrip RubyMine AppCode"
 
-if [ $OS_NAME == "Darwin" ]; then
+if [ "$OS_NAME" = "Darwin" ]; then
 	echo 'macOS:'
 
 	for PRD in $JB_PRODUCTS; do
-    	rm -rf ~/Library/Preferences/${PRD}*/eval
-    	rm -rf ~/Library/Application\ Support/${PRD}*/eval
+    rm -rf ~/Library/Preferences/${PRD}*/eval
+    rm -rf ~/Library/Application\ Support/${PRD}*/eval
+    rm -rf ~/Library/Application\ Support/JetBrains/${PRD}*/eval
 	done
-elif [ $OS_NAME == "Linux" ]; then
+elif [ "$OS_NAME" = "Linux" ]; then
 	echo 'Linux:'
 
 	for PRD in $JB_PRODUCTS; do
-    	rm -rf ~/.${PRD}*/config/eval
+	  rm -rf ~/.${PRD}*/config/eval
+    rm -rf ~/.config/${PRD}*/eval
 	done
 else
-	echo 'unsupport'
+	echo 'unsupported'
 	exit
 fi
 
